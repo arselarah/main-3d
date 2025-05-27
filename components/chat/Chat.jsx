08cea1d80c1d9073684a017be247b0b9cf1ec3ba
Diego Sabillon <117101226+DiegoS2004@users.noreply.github.com>
2025-05-26 22:23:35 -0600

    Update Chat.jsx
    to api in render

--- components/chat/Chat.jsx
+++ components/chat/Chat.jsx
@@ -114,7 +114,7 @@ export default function Chat() {
     setMenuVisible(false);
     setLoading(true);
     try {
-      const res = await fetch('http://127.0.0.1:8000/chat', {
+      const res = await fetch('https://main3d-api-rag.onrender.com/chat', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ pregunta: message, reiniciar: false })
